561ce1eb551ab2ee73c40caf993f981a19da8cd8
couchds <danielcouch1864@gmail.com>
2023-03-11 01:58:03 -0500

    Start experimenting with Material UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import { Container } from '@mui/material';
+import { AppBar } from '@mui/material';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,29 +17,12 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
+        <Container>
+          <AppBar>
+            Couch Room
+          </AppBar>
+          <h1>Hello, this is my personal website. We&apos;ll see what happens with it!</h1>
+        </Container>
       </main>
     </>
   )
